63ec814fe42643ea3d09a901524fd7e0f5785081
Jason King <jpk2552@gmail.com>
2025-07-13 16:03:59 -0400

    add connection for acceptance beliefs

--- src/App/SupportConnection/index.tsx
+++ src/App/SupportConnection/index.tsx
@@ -14,38 +14,80 @@ export default ({ fromX, fromY, toX, toY }) => {
   // TODO change circle to be fake node (temp border) (should be red if not connectingUser)
   // TODO should change sides based on connectingUser
 
-  const [edgePath] = getBezierPath({
-    sourceX: fromX,
-    sourceY: fromY,
-    targetX: toX,
-    targetY: toY,
-    sourcePosition: fromHandle?.position,
-    targetPosition: nodeData.connectingUser
-      ? nodeData.connectingUser === "left"
-        ? Position.Left
-        : Position.Right
-      : Position.Top, // TODO check if this is correct
-  });
+  console.log("connection x y ", fromX, fromY, toX, toY);
 
-  return (
-    <g>
-      <path
-        fill="none"
-        stroke={nodeData?.connectingUser ? "green" : "red"}
-        opacity={nodeData?.connectingUser ? 1 : 0.5}
-        strokeWidth={1.5}
-        className="animated" // TODO not animated when not connectingUser
-        style={{ animationDirection: "reverse" }} // TODO check on the type
-        d={edgePath} //{`M${fromX},${fromY} C ${fromX} ${toY} ${fromX} ${toY} ${toX},${toY}`}
-      />
-      <circle
-        cx={toX}
-        cy={toY}
-        fill="#fff"
-        r={3}
-        stroke={"white"}
-        strokeWidth={1.5}
-      />
-    </g>
-  );
+  if (fromHandle?.type == "target") {
+    // Connection for creating a new parent needs to account for user being selected
+    const [edgePath] = getBezierPath({
+      sourceX: fromX,
+      sourceY: fromY,
+      targetX: toX,
+      targetY: toY,
+      sourcePosition: fromHandle?.position,
+      targetPosition: nodeData.connectingUser
+        ? nodeData.connectingUser === "left"
+          ? Position.Left
+          : Position.Right
+        : Position.Top,
+    });
+
+    return (
+      <g>
+        <path
+          fill="none"
+          stroke={nodeData?.connectingUser ? "green" : "red"}
+          opacity={nodeData?.connectingUser ? 1 : 0.5}
+          strokeWidth={1.5}
+          className="animated" // TODO not animated when not connectingUser
+          style={{ animationDirection: "reverse" }} // TODO check on the type
+          d={edgePath} //{`M${fromX},${fromY} C ${fromX} ${toY} ${fromX} ${toY} ${toX},${toY}`}
+        />
+        <circle
+          cx={toX}
+          cy={toY}
+          fill="#fff"
+          r={3}
+          stroke={"white"}
+          strokeWidth={1.5}
+        />
+      </g>
+    );
+  } else if (fromHandle?.type == "source") {
+    // Connection for creating a new child belief needs to account for source Handle
+
+    const [edgePath] = getBezierPath({
+      // TODO this is half the width of the AcceptanceHandle
+      sourceX: fromHandle.position == Position.Left ? fromX - 17 : fromX + 17,
+      sourceY: fromY,
+      targetX: toX,
+      targetY: toY,
+      sourcePosition: fromHandle?.position,
+      targetPosition: Position.Top, // Always target the top for child beliefs
+    });
+
+    return (
+      <g>
+        <path
+          fill="none"
+          stroke={"white"}
+          opacity={1}
+          strokeWidth={1.5}
+          className="animated"
+          style={{ animationDirection: "reverse" }}
+          d={edgePath}
+        />
+        <circle
+          cx={toX}
+          cy={toY}
+          fill="#fff"
+          r={3}
+          stroke={"white"}
+          strokeWidth={1.5}
+        />
+      </g>
+    );
+  } else {
+    // If no handle type, return nothing
+    return null;
+  }
 };
